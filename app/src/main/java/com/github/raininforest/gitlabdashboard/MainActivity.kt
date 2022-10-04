75c74c74d063b8302d8504ffb07b607b874a9ed2
Sergey Velesko <velesko@alkor.co.ru>
2022-10-04 19:19:40 +0300

    added previews for all screens

--- app/src/main/java/com/github/raininforest/gitlabdashboard/MainActivity.kt
+++ app/src/main/java/com/github/raininforest/gitlabdashboard/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.raininforest.gitlabdashboard.ui.theme.GitlabDashboardTheme
+import com.github.raininforest.login.ui.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +19,20 @@ class MainActivity : ComponentActivity() {
             GitlabDashboardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GitlabDashboardTheme {
-        Greeting("Android")
+        //LoginScreen()
+        //HomeScreen()
+        //MrDashboardScreen()
     }
 }
