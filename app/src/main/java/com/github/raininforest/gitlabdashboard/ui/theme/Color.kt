bf6427959c2343df00f54a080d65f833ec95ddaf
Sergey Velesko <velesko@alkor.co.ru>
2022-10-04 19:17:13 +0300

    set colors

--- app/src/main/java/com/github/raininforest/gitlabdashboard/ui/theme/Color.kt
+++ app/src/main/java/com/github/raininforest/gitlabdashboard/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.github.raininforest.gitlabdashboard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Primary = Color(0xFF2196F3)
+val PrimaryVariant = Color(0xFF3F51B5)
+val Secondary = Color(0xFF009688)
