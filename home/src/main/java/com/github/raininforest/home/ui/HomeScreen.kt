e54d2024677400025e936e20c20efe5c5361c740
Sergey Velesko <velesko@alkor.co.ru>
2022-10-04 19:18:49 +0300

    implemented home screen ui

---
+++ home/src/main/java/com/github/raininforest/home/ui/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.github.raininforest.home.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    val padding = 16.dp
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = PaddingValues(padding),
+        verticalArrangement = Arrangement.spacedBy(padding),
+        horizontalArrangement = Arrangement.spacedBy(padding)
+    ) {
+        items(listOf("TOP-5 users with high MR count", "empty", "empty", "empty", "empty", "empty")) {
+            DashboardItem(text = it) {}
+        }
+    }
+}
+
+@Composable
+fun DashboardItem(text: String, onClick: () -> Unit) {
+    OutlinedButton(
+        modifier = Modifier
+            .defaultMinSize(minWidth = 192.dp, minHeight = 192.dp),
+        shape = RoundedCornerShape(16.dp),
+        content = {
+            Text(text, textAlign = TextAlign.Center)
+        },
+        onClick = onClick
+    )
+}
