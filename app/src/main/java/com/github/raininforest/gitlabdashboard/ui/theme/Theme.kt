77742d5c1012d7869adbc228475afd1f0a8c4d81
Sergey Velesko <velesko@alkor.co.ru>
2022-10-04 19:18:29 +0300

    setup theme

--- app/src/main/java/com/github/raininforest/gitlabdashboard/ui/theme/Theme.kt
+++ app/src/main/java/com/github/raininforest/gitlabdashboard/ui/theme/Theme.kt
@@ -1,21 +1,26 @@
 package com.github.raininforest.gitlabdashboard.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Primary,
+    primaryVariant = PrimaryVariant,
+    secondary = Secondary
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Primary,
+    primaryVariant = PrimaryVariant,
+    secondary = Secondary
 
     /* Other default colors to override
     background = Color.White,
@@ -33,6 +38,16 @@ fun GitlabDashboardTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @C
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insets = WindowCompat.getInsetsController(window, view)
+            window.statusBarColor = colors.primary.toArgb()
+            insets.isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
